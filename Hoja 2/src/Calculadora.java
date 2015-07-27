4b2900c5ce48a1d3371b4d35142207be374374fc
diego-rc <diego.rc.91@gmail.com>
2015-07-27 08:01:16 -0600

    codeigo1

---
+++ Hoja 2/src/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author diego_000
+ */
+public interface Calculadora {
+    
+    public void setString(String linea);
+    
+    public void meterVector();
+    
+    public boolean calcularVector();
+    
+    public int getResultado();
+    
+}
---
+++ Hoja 2/src/Stack.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * An interface describing a Last-In, First-Out structure.
+ * Stacks are typically used to store the state of a recursively
+ * solved problem.
+ * The structure package provides several implementations of the Stack interface, 
+ * each of which has its particular strengths and weaknesses.
+ *  
+ * <P>
+ * Example usage:
+ * <P>
+ * To reverse a string using a stack, we would use the following:
+ * <pre>
+ * public static void main(String[] arguments)
+ * {
+ *     if(arguments.length > 0){
+ *         {@link Stack} reverseStack = new {@link structure.StackList#StackList() StackList()};
+ *         String s = arguments[0];
+ *          
+ *         for(int i=0; i < s.length(); i++){
+ *             reverseStack.{@link #push(Object) push(new Character(s.charAt(i)))};
+ *         }
+ *
+ *         while(!reverseStack.{@link #empty()}){
+ *             System.out.print(reverseStack.{@link #pop()});
+ *         }
+ *
+ *         System.out.println();
+ *     }
+ * }
+ * </pre>
+ * @version $Id: Stack.java 22 2006-08-21 19:27:26Z bailey $
+ * @author, 2001 duane a. bailey
+ */
+public interface Stack<E> {
+
+    /**
+     * Add an element to top of stack.
+     *
+     * @post item is added to stack
+     *       will be popped next if no intervening push
+     * 
+     * @param item The value to be added to the top of the stack.
+     */
+    public void push(E item);
+
+    /**
+     * Remove an element from the top of the stack.
+     *
+     * @pre stack is not empty
+     * @post most recently pushed item is removed and returned
+     * 
+     * @return A reference to the removed element.
+     */
+    public E pop();
+
+    /**
+     * Fetch a reference to the top element of the stack.
+     *
+     * @pre stack is not empty
+     * @post top value (next to be popped) is returned
+     * 
+     * @return A reference to the top element of the stack.
+     */
+    public E peek();
+
+    /**
+     * Returns true iff the stack is empty.  Provided for
+     * compatibility with java.util.Vector.empty.
+     *
+     * @post returns true if and only if the stack is empty
+     * 
+     * @return True iff the stack is empty.
+     */
+    public boolean empty();
+
+    /**
+     * Returns the number of elements in the stack.
+     *
+     * @post returns the number of elements in the stack
+     * @return number of elements in stack.
+     */
+    public int size();
+}
