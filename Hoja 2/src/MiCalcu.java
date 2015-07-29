011aac4ffc7f8eb8b63d6ec7530f50ee6c291ca5
diego-rc <diego.rc.91@gmail.com>
2015-07-29 08:27:37 -0600

    Implementacion completa de la Calculadora

--- Hoja 2/src/MiCalcu.java
+++ Hoja 2/src/MiCalcu.java
@@ -13,6 +13,7 @@ public class MiCalcu implements Calculadora {
     
     protected String cadena;
     protected Stack<Integer> data;
+    protected boolean error;
     
     public MiCalcu(){
         data=new StackVector<>();
@@ -34,7 +35,67 @@ public class MiCalcu implements Calculadora {
     
     @Override
     public void meterVector(){
-        this.calcularVector();
+        error=this.calcularVector();
+        if(!error){
+            System.out.println("Error");
+            System.out.print("Valores en el Stack"+data);
+        }
+    }
+    
+    @Override
+    public boolean calcularVector(){
+        boolean flag=true;
+        
+        cadena.replace(" ","");
+        for(int i=0; i==cadena.length(); i++){
+            String car=cadena.substring(i, i+1);
+            if (car.equals("0")||car.equals("1")||car.equals("2")||car.equals("3")||car.equals("4")||car.equals("5")||car.equals("6")||car.equals("7")||car.equals("8")||car.equals("9")){
+                data.push(Integer.parseInt(car));
+            }
+            else if(car.equals("*")||car.equals("+")||car.equals("-")||car.equals("/")){
+                if (data.size()<2){
+                    flag=false;
+                    i=cadena.length();
+                }
+                else{
+                    int dat1;
+                    int dat2;
+                    dat2=data.pop();
+                    dat1=data.pop();
+                    if(car.equals("+")){
+                        data.push(dat1+dat2);
+                    }
+                    else if(car.equals("*")){
+                        data.push(dat1*dat2);
+                    }
+                    else if(car.equals("-")){
+                        data.push(dat1-dat2);
+                    }
+                    else if(car.equals("/")){
+                        if(dat2==0){
+                            flag=false;
+                            i=cadena.length();
+                            data.push(dat1);
+                            data.push(dat2);
+                        }
+                        else{
+                            data.push(dat1/dat2);
+                        }
+                    }
+                    
+                }
+            }
+            else{
+                flag=false;
+                i=cadena.length();
+            }
+        }
+        
+        if(data.size()>1){
+            System.out.println("Advertencia: quedaron valores sin operar en la calculadora");
+        }
+        
+        return flag;
     }
     
 }
